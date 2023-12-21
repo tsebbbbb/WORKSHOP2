5649551e1ced7d02e730fa5b43d3f8d5238f631f
tsebbbbb <65160209@go.buu.ac.th>
2023-12-21 17:33:27 +0700

    1

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <html>
+    <head>
+        <meta charset = "utf-8">
+    </head>
     <body>
-        <h1>This is PHP</h1>
+        <h1><?php echo"Hello World<br><br>" ?></h1>
+        <?php echo "<h1>Hello World<br><br>"
     </body>
 </html>
