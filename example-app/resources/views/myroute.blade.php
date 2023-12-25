38906648b1b1b9b6b1f94910b5b4a9e6bf5dbf48
tsebbbbb <65160209@go.buu.ac.th>
2023-12-25 18:33:50 +0700

    1

--- example-app/resources/views/myroute.blade.php
+++ example-app/resources/views/myroute.blade.php
@@ -3,9 +3,9 @@
         <meta charset = "utf-8" />
     </head>
     <style>
-        
+
     </style>
     <body>
-        <h1>My Route Page</h1>
+        <h1>My Route Pageggtgtggt</h1>
     </body>
 </html>
