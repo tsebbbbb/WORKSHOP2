f0be2156eaa1ad0ed07a04cbc20f23185747886f
tsebbbbb <65160209@go.buu.ac.th>
2023-12-25 17:08:08 +0700

    1

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <html>
-    <head>
-        <meta charset = "utf-8">
-    </head>
-    <body>
-        <h1><?php echo"Hello World<br><br>" ?></h1>
-        <?php echo "<h1>Hello World<br><br></h1>" ?>
-         
-    </body>
+     
+
+<head >
+    <meta charset="utf-8">
+    <style>
+        div {
+            width: 200px;
+            height: 380px;
+            border: 1px solid black;
+            overflow: scroll;
+            }
+    </style>
+</head>
+<center>
+<body>
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+    <div> 
+        <?php for($i = 1; $i <= 24; $i++) {?>
+            <?php echo $multi_x ?> x <?php echo $i ?> = <?php echo $multi_x*$i ?><br>
+        <?php } ?>
+    </div>
+</body>
+    </center>
+
+
 </html>
